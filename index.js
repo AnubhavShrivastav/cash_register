1c1df0d714fe26cd8136b4136f8a6973e551af5a
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2024-10-18 13:15:38 +0530

    add comments

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ const availableNotes = [500,100,50,20,10,5,1]
 function clickHandler (){
     hideMessage();
 
-    if(billAmount.value > 0){
-       if(cashGiven.value >= billAmount.value){
-       const amountToBeReturned = cashGiven.value - billAmount.value;
-       calculateChange(amountToBeReturned)
+    /* suppose 
+     billAmount  = 1225
+     caseGiven = 2000
+    */
+
+    if(billAmount.value > 0){ // 1225 > 0 = true
+       if(cashGiven.value >= billAmount.value){ // 2000 >= 1225 = true
+       const amountToBeReturned = cashGiven.value - billAmount.value; // 2000 - 1225 = 775 
+       calculateChange(amountToBeReturned) // call a function  ex - calculateChange(775)
        }else{
         showMessage("Do you wanna wash plates?")
        }
@@ -22,10 +27,21 @@ function clickHandler (){
     }
 }
 
-function calculateChange(amountToBeReturned){
+function calculateChange(amountToBeReturned){ //775
+    // go over all the available notes
     for(let i = 0; i < availableNotes.length; i++){
+
+    // no of notes need for the denomination  
       const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+      // 775 / 500 = 1.55 || 275 / 100 = 2 || 75 / 50 = 1 || 25 / 20 = 1 || 5 / 10 = 0 || 5 / 5 = 1
+      // math trunc convert 1.55 to 1, it remove all number after decimal point.
+
+      // amount left after calculating the number of notes needed
       amountToBeReturned =   amountToBeReturned % availableNotes[i];
+      //775 % 500 = 275 || 275 % 100 = 75 || 75 % 50 = 25 || 25 % 20 = 5 ||  5 % 5 = 0
+      
+
+      //updating the no of notes in the table for current amount
       noOfNotes[i].innerText = numberOfNotes;
 
     }
