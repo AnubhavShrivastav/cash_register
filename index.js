315f67ef43e2cde2436e37a9ddb26e86852dd832
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2024-10-18 12:01:42 +0530

    create hideMessage function

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ function clickHandler (){
     hideMessage();
 
     if(billAmount.value > 0){
-       if(cashGiven.value > billAmount.value){
+       if(cashGiven.value >= billAmount.value){
        const amountToBeReturned = cashGiven.value - billAmount.value;
        calculateChange(amountToBeReturned)
-
        }else{
         showMessage("Do you wanna wash plates?")
        }
@@ -23,19 +22,20 @@ function clickHandler (){
     }
 }
 
-function hideMessage(){
-    message.style.display = "none";
-}
-
 function calculateChange(amountToBeReturned){
     for(let i = 0; i < availableNotes.length; i++){
       const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-      amountToBeReturned % availableNotes[i];
+      amountToBeReturned =   amountToBeReturned % availableNotes[i];
       noOfNotes[i].innerText = numberOfNotes;
 
     }
 
 }
+
+function hideMessage(){
+    message.style.display = "none";
+}
+
 
 function showMessage(msg){
  message.style.display = "block";
